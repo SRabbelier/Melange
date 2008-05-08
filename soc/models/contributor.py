e1214a28e4b231998b1528fa52b8660c6dcc3e91
Sverre Rabbelier <srabbelier@gmail.com>
2008-05-08 23:48:43 +0000

    Initial implementation of the Contributor Model in the contributor.py module.
    Review: http://codereviews.googleopensourceprograms.com/61
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%40306

---
+++ soc/models/contributor.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2008 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the Contributor Model."""
+
+__authors__ = [
+  '"Todd Larsen" <tlarsen@google.com>',
+  '"Sverre Rabbelier" <sverre@rabbelier.nl>',
+]
+
+from google.appengine.ext import db
+
+from soc import models
+import soc.models.author
+
+
+class Contributor(db.Model):
+  """Contributor details for a specific Program.
+
+  Some Contributor workflows have the Contributor (acting as an Author)
+  creating Proposals and desiring for one (or more?) of them to be
+  converted into Tasks by Reviewers and Hosts.  Other workflows have the
+  Reviewers (acting as an Author) proposing Proposals, that Contributors
+  claim to convert them into Tasks.
+
+  A Contributor entity participates in the following relationships implemented 
+  as a db.ReferenceProperty elsewhere in another db.Model:
+
+   tasks)  a many:many relationship associating all of the Tasks to which
+     a specific Contributor has contributed with that Contributor.  See
+     the TasksContributors model for details.
+  """
+  
+  #: a 1:1 relationship associating a Contributor with generic Author
+  #: details and capabilities. The back-reference in the Author
+  #: model is a Query named 'contributor'.
+  author = db.ReferenceProperty(reference_class=models.author.Author, 
+                                required=True, 
+                                collection_name="contributor")

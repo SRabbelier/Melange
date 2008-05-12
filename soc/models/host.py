3172487b8ccd5820898857c899cc3d9c75de3c5d
Sverre Rabbelier <srabbelier@gmail.com>
2008-05-12 19:34:58 +0000

    Initial definition of the Host Model.
    Patch by: Sverre Rabbelier
    Review by: Todd Larsen
    Review issue: 101
    Review URL: http://codereviews.googleopensourceprograms.com/101
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%40321

---
+++ soc/models/host.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2008 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the Host Model."""
+
+__authors__ = [
+  '"Todd Larsen" <tlarsen@google.com>',
+  '"Sverre Rabbelier" <sverre@rabbelier.nl>',
+]
+
+from google.appengine.ext import db
+
+from soc import models
+import soc.models.administrator
+
+
+class Host(db.Model):
+  """Host details for a specific Program."""
+
+  #: A 1:1 relationship associating a Host with more generic
+  #: Administrator details and capabilities.  The back-reference in
+  #: the Administrator model is a Query named 'host'.
+  admin = db.ReferenceProperty(reference_class=models.administrator.Administrator, 
+                               required=True, collection_name="host")
+

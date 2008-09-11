0a9f280b7dc513643e588a8c635fc74ef33d9cbd
Sverre Rabbelier <srabbelier@gmail.com>
2008-09-11 21:30:08 +0000

    Updated the script to: * Include a comment describing what it does * Use variables for anything non-standard * Use the --parse-only flag of epydoc * Exclude all 'django' modules through --exclude= * Run on '../app' by default, no more arguments needed * Clean up the output directory before running
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%40553

--- scripts/build_epydocs.sh
+++ scripts/build_epydocs.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+# Generates epydoc for $target to $outdir, while ignoring $exclude
+# WARNING: The contents of $outdir are -deleted- before the running epydoc
+# This way there are no 'stale files' when the script finishes.
+
+ outdir="../../wiki/html/epydoc"
+ target="../app ../tests"
+exclude="django"
+
+echo "Cleaning out $outdir..."
+rm -f $outdir/*
+echo "Done."
+echo
 
 echo "Running epydoc..."
-echo $1
+echo "$target -> $outdir"
+echo "================="
 
 epydoc --html -v --show-private --inheritance=included --graph=all \
-  -o ../../wiki/html/epydoc $1
+  --parse-only --exclude=$exclude -o $outdir $target
 
+echo "================="
 echo "Done."
+echo
+
+echo "Goodbye."
 

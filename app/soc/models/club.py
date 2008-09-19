eba5bb07a67686df74005d6b0683ae11fc7ff2e4
Todd Larsen <tlarsen@google.com>
2008-09-19 17:47:21 +0000

    First pass (from some old code notes) of a Club Model (a form of Group).
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%40598

---
+++ app/soc/models/club.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2008 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the Club Model."""
+
+__authors__ = [
+  '"Todd Larsen" <tlarsen@google.com>',
+]
+
+
+from google.appengine.ext import db
+
+from django.utils.translation import ugettext_lazy
+
+from soc.models import base
+import soc.models.group
+
+
+class Club(base.ModelWithFieldAttributes):
+  """Details specific to a Club.
+
+  A Club is a generic type of Group that gathers Members into a community.
+
+  A Club entity participates in the following relationships implemented
+  as a db.ReferenceProperty elsewhere in another db.Model:
+
+   members)  a 1:many relationship of Members belonging to a Club.  This
+     relation is implemented as the 'members' back-reference Query of the
+     Member model 'club' reference.
+  """
+
+  #: Required 1:1 relationship that ties Club details to the more generic
+  #: Group details.  The back-reference in the Group model is a Query
+  #: named 'club'.
+  group = db.ReferenceProperty(reference_class=soc.models.org.Group,
+                               required=True, collection_name="club")

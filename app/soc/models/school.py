31d729b9c6f9bacbc80e13bb417705bd5d19fe1d
Todd Larsen <tlarsen@google.com>
2008-09-19 17:51:33 +0000

    First pass (from some old code notes) of a School Model (a form of Group).
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%40599

---
+++ app/soc/models/school.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2008 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the School Model."""
+
+__authors__ = [
+  '"Todd Larsen" <tlarsen@google.com>',
+]
+
+
+from google.appengine.ext import db
+
+from django.utils.translation import ugettext_lazy
+
+from soc.models import base
+import soc.models.group
+
+
+class School(base.ModelWithFieldAttributes):
+  """Details specific to a School.
+
+  A School is a specific type of Group that gathers Students together.
+
+  A School entity participates in the following relationships implemented
+  as a db.ReferenceProperty elsewhere in another db.Model:
+
+   students)  a 1:many relationship of Students attending (or otherwise
+     belonging to) a School.  This relation is implemented as the 'students'
+     back-reference Query of the Student model 'school' reference.
+  """
+
+  #: Required 1:1 relationship that ties School details to the more
+  #: generic Group details.  The back-reference in the Group model is a
+  #: Query named 'school'.
+  group = db.ReferenceProperty(reference_class=soc.models.group.Group,
+                               required=True, collection_name="school")

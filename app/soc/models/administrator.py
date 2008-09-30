c79ce18e8ba23f09fdfce472387ffcdbabdbba2f
Pawel Solyga <Pawel.Solyga@gmail.com>
2008-09-30 14:00:11 +0000

    Add org ReferenceProperty to Administrator model (many:1 relationship associating Administrators with specific Organization). Update Organization model __doc__ string with information about  the back-reference 'admins'.
    Patch by: Pawel Solyga
    Review by: to-be-reviewed
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%40644

--- app/soc/models/administrator.py
+++ app/soc/models/administrator.py
@@ -24,11 +24,17 @@ __authors__ = [
 
 from google.appengine.ext import db
 
+from soc.models import organization
+
 import soc.models.reviewer
 
 
 class Administrator(soc.models.reviewer.Reviewer):
   """Administrator details for a specific Program.
   """
-  pass
+  #: A many:1 relationship associating Administrators with specific
+  #: Organization details and capabilities. The back-reference in the
+  #: Organization model is a Query named 'admins'.
+  org = db.ReferenceProperty(reference_class=organization.Organization,
+          required=True, collection_name="admins")
 

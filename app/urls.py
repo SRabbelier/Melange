5a13c64d068be08f35431f0bdb9fd89975b33046
Todd Larsen <tlarsen@google.com>
2008-10-04 07:22:43 +0000

    Call soc.logic.site.map.getDjangoUrlPatterns() to construct the urlpatterns from the site map, rather than explicitly listing them in app/urls.py.
    Patch by: Todd Larsen
    Review by: to-be-reviewed
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%40717

--- app/urls.py
+++ app/urls.py
@@ -14,69 +14,12 @@
 
 __authors__ = [
   '"Augie Fackler" <durin42@gmail.com>',
+  '"Todd Larsen" <tlarsen@google.com>',
   '"Pawel Solyga" <pawel.solyga@gmail.com>',
   ]
 
 
-from django.conf.urls.defaults import *
-
-from soc.logic import path_linkname
+from soc.logic.site import map
 
 
-urlpatterns = patterns(
-    # Home Page view
-    '',
-    (r'^$', 'soc.views.site.home.public'),
-    
-    # Home Page Site views
-    (r'^site/home$', 'soc.views.site.home.public'),
-    (r'^site/home/edit$', 'soc.views.site.home.edit'),
-
-    # TODO(tlarsen): uncomment these when the view functions are committed
-    # attempt to send User to their dashboard
-    # (will display soc.views.user.roles.public() if "linkname" is not
-    # current logged-in User)    
-    # (r'^user/roles$',
-    #  'soc.views.user.roles.dashboard'),
-    # (r'^user/roles/(?P<linkname>[_0-9a-z]+)$',
-    #  'soc.views.user.roles.dashboard'),
-    
-    # User Profile Site views
-    (r'^site/user/lookup$', 'soc.views.site.user.profile.lookup'),
-    (r'^site/user/profile$', 'soc.views.site.user.profile.create'),
-    (r'^site/user/profile/%s$' % path_linkname.LINKNAME_ARG_PATTERN,
-     'soc.views.site.user.profile.edit'),
-    (r'^site/user/list$', 'soc.views.site.user.list.all'),
-
-    # Document views
-    (r'^docs/show/%s$' % path_linkname.PATH_LINKNAME_ARGS_PATTERN,
-     'soc.views.docs.show.public'),
-    
-    # Document Site views
-    (r'^site/docs/edit$', 'soc.views.site.docs.edit.create'),
-    (r'^site/docs/edit/%s$' % path_linkname.PATH_LINKNAME_ARGS_PATTERN,
-     'soc.views.site.docs.edit.edit'),
-    (r'^site/docs/list$', 'soc.views.site.docs.list.all'),
-    
-    # Sponsor Group public view
-    (r'^sponsor/profile/%s' % path_linkname.LINKNAME_ARG_PATTERN,
-     'soc.views.sponsor.profile.public'),
-    
-    # Sponsor Group Site views
-    (r'^site/sponsor/profile$', 'soc.views.site.sponsor.profile.edit'),
-    (r'^site/sponsor/profile/%s' % path_linkname.LINKNAME_ARG_PATTERN,
-     'soc.views.site.sponsor.profile.edit'),
-    (r'^site/sponsor/list$', 'soc.views.site.sponsor.list.all'),
-    
-    # User Profile views
-    (r'^user/profile$', 'soc.views.user.profile.edit'),
-    (r'^user/profile/%s$' % path_linkname.LINKNAME_ARG_PATTERN,
-     'soc.views.user.profile.edit'),
-
-    # these are not really used...
-    (r'^org/profile/(?P<program>ghop[_0-9a-z]+)/(?P<linkname>[_0-9a-z]+)/$',
-     'soc.views.person.profile.edit',
-     {'template': 'ghop/person/profile/edit.html'}),
-    (r'^org/profile/(?P<program>[_0-9a-z]+)/(?P<linkname>[_0-9a-z]+)/$',
-     'soc.views.person.profile.edit'),
-)
+urlpatterns = map.getDjangoUrlPatterns()

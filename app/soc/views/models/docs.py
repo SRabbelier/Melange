abfec953c5f56207f2a170c2c7e5dece74605e45
Sverre Rabbelier <srabbelier@gmail.com>
2008-11-12 21:43:15 +0000

    Fixed typo in docstring
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%401017

--- app/soc/views/models/docs.py
+++ app/soc/views/models/docs.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Views for Sponsor profiles.
+"""Views for Documents.
 """
 
 __authors__ = [

df544db17610c069d32a72f17da40da20e350ac4
Sverre Rabbelier <srabbelier@gmail.com>
2009-01-20 00:48:16 +0000

    Fixed whitespace and EOL damage
    Patch by: Sverre Rabbelier
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%401411

--- app/soc/models/notification.py
+++ app/soc/models/notification.py
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,18 +39,18 @@ class Notification(soc.models.linkable.Linkable):
       required=False,
       collection_name="sent_notifications",
       verbose_name=ugettext_lazy('From'))
-  
+
   subject = db.StringProperty(required=True,
       verbose_name=ugettext_lazy('Subject'))
-  
+
   #: the message that is contained within this Notification
   message = db.TextProperty(required=True,
       verbose_name=ugettext_lazy('Message'))
-  
+
   #: date and time on which this Notification was created
   created_on = db.DateTimeProperty(auto_now_add=True,
       verbose_name=ugettext_lazy('Created On'))
-  
+
   #: boolean property that marks if the notification is unread
   unread = db.BooleanProperty(default=True,
       verbose_name=ugettext_lazy('Unread'))

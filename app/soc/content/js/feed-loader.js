f13384da5ff58f5e9028df0eedbebc00f8e39b02
Sverre Rabbelier <srabbelier@gmail.com>
2009-02-19 20:13:41 +0000

    Fixed blog on home view
    For some reason the script does not work if it resides in it's own
    file, perhaps some JS guru can enlighten?
    
    Patch by: Sverre Rabbelier
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%402027

--- app/soc/content/js/feed-loader.js
+++
@@ -1,8 +0,0 @@
-google.load("feeds", "1");
-
-function initialize() {
-  var blog = new BlogPreview(document.getElementById("blog"));
-  blog.show("{{ entity.feed_url }}", 3);
-}
-google.setOnLoadCallback(initialize);
-

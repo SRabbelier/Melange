297eaa9e70d411975474e2f4ddca4c513365519d
Sverre Rabbelier <srabbelier@gmail.com>
2009-02-19 21:10:09 +0000

    Added OSI approved licenses
    Patch by: Sverre Rabbelier
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%402032

---
+++ app/soc/models/licenses.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2008 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Set of licenses that have been approved by the OSI.
+"""
+
+__authors__ = [
+  '"Sverre Rabbelier" <sverre@rabbelier.nl>',
+]
+
+
+LICENSES = (
+  'Academic Free License 3.0 (AFL 3.0)',
+  'Affero GNU Public License',
+  'Adaptive Public License',
+  'Apache License, 2.0',
+  'Apple Public Source License',
+  'Artistic license 2.0',
+  'Attribution Assurance Licenses',
+  'New and Simplified BSD licenses',
+  'Boost Software License (BSL1.0)',
+  'Computer Associates Trusted Open Source License 1.1',
+  'Common Development and Distribution License',
+  'Common Public Attribution License 1.0 (CPAL)',
+  'Common Public License 1.0',
+  'CUA Office Public License Version 1.0',
+  'EU DataGrid Software License',
+  'Eclipse Public License',
+  'Educational Community License, Version 2.0',
+  'Eiffel Forum License V2.0',
+  'Entessa Public License',
+  'Fair License',
+  'Frameworx License',
+  'GNU General Public License (GPL)',
+  'GNU General Public License version 3.0 (GPLv3)',
+  'GNU Library or "Lesser" General Public License (LGPL)',
+  'GNU Library or "Lesser" General Public License version 3.0 (LGPLv3)',
+  'Historical Permission Notice and Disclaimer',
+  'IBM Public License',
+  'ISC License',
+  'Lucent Public License Version 1.02',
+  'Microsoft Public License (Ms-PL)',
+  'Microsoft Reciprocal License (Ms-RL)',
+  'MIT license',
+  'Motosoto License',
+  'Mozilla Public License 1.1 (MPL)',
+  'Multics License',
+  'NASA Open Source Agreement 1.3',
+  'NTP License',
+  'Naumen Public License',
+  'Nethack General Public License',
+  'Nokia Open Source License',
+  'Non-Profit Open Software License 3.0 (Non-Profit OSL 3.0)',
+  'OCLC Research Public License 2.0',
+  'Open Group Test Suite License',
+  'Open Software License 3.0 (OSL 3.0)',
+  'PHP License',
+  'Python license',
+  'Python Software Foundation License',
+  'Qt Public License (QPL)',
+  'RealNetworks Public Source License V1.0',
+  'Reciprocal Public License 1.5 (RPL1.5)',
+  'Ricoh Source Code Public License',
+  'Simple Public License 2.0',
+  'Sleepycat License',
+  'Sun Public License',
+  'Sybase Open Watcom Public License 1.0',
+  'University of Illinois/NCSA Open Source License',
+  'Vovida Software License v. 1.0',
+  'W3C License',
+  'wxWindows Library License',
+  'X.Net License',
+  'Zope Public License',
+  'zlib/libpng license',
+)

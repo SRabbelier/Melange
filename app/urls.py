746d78ed105ff685dc090aba9cb0b41f7a4e363c
Pawel Solyga <Pawel.Solyga@gmail.com>
2009-04-03 17:10:32 +0000

    Add missing docstring url.py.
    Patch by: Pawel Solyga
    Reviewed by: to-be-reviewed
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%402806

--- app/urls.py
+++ app/urls.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Module containing Melange URL patterns definition.
+"""
+
 __authors__ = [
   '"Augie Fackler" <durin42@gmail.com>',
   '"Todd Larsen" <tlarsen@google.com>',

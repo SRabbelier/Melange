14ea9770f2163138e5c51be94155cdf32ac618cd
Sverre Rabbelier <srabbelier@gmail.com>
2009-04-18 14:02:56 +0000

    Add priority groups (model, logic, and view)
    Priority groups will be a key part of the Job system, allowing the
    site admin to give a higher priority to certain job types on the fly.
    
    Patch by: Sverre Rabbelier
    
    --HG--
    extra : convert_revision : svn%3A32761e7d-7263-4528-b7be-7235b26367ec/trunk%402967

---
+++ app/soc/logic/models/priority_group.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2009 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Priority Group (Model) query functions.
+"""
+
+__authors__ = [
+  '"Sverre Rabbelier" <sverre@rabbelier.nl>',
+  ]
+
+
+from soc.logic.models import base
+
+import soc.models.priority_group
+
+
+class Logic(base.Logic):
+  """Logic methods for the Priority Group model.
+  """
+
+  def __init__(self, model=soc.models.priority_group.PriorityGroup,
+               base_model=None, scope_logic=None):
+    """Defines the name, key_name and model for this entity.
+    """
+
+    self.EMAIL = 'emails'
+    self.CONVERT = 'convert'
+
+    self.groups = {
+        self.EMAIL: 'Send out emails',
+        self.CONVERT: 'Convert one entity to another type',
+        }
+
+    super(Logic, self).__init__(model=model, base_model=base_model,
+                                scope_logic=scope_logic)
+
+  def getKeyValuesFromEntity(self, entity):
+    """See base.Logic.getKeyValuesFromEntity.
+    """
+
+    return [entity.link_id]
+
+  def getKeyValuesFromFields(self, fields):
+    """See base.Logic.getKeyValuesFromFields.
+    """
+
+    return [fields['link_id']]
+
+  def getKeyFieldNames(self):
+    """See base.Logic.getKeyFieldNames.
+    """
+
+    return ['link_id']
+
+  def getGroup(self, key_name):
+    """Return the specified Priority Group entity.
+    """
+
+    if key_name not in self.groups:
+      raise base.InvalidArgumentError("Unknown priority group %s." % key_name)
+
+    group = self.getFromKeyName(key_name)
+
+    if not group:
+      fields = {
+          'link_id': key_name,
+          'name': self.groups[key_name],
+          'priority': 0,
+          }
+
+      group = self.updateOrCreateFromKeyName(fields, key_name)
+
+    return group
+
+
+logic = Logic()
---
+++ app/soc/views/models/priority_group.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2009 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Views for Priority Groups.
+"""
+
+__authors__ = [
+    '"Sverre Rabbelier" <sverre@rabbelier.nl>',
+  ]
+
+
+from django import forms
+
+from soc.logic import cleaning
+from soc.logic import dicts
+from soc.logic.models.priority_group import logic as priority_group_logic
+from soc.views.helper import access
+from soc.views.helper import decorators
+from soc.views.helper import dynaform
+from soc.views.helper import widgets
+from soc.views.models import base
+
+
+class View(base.View):
+  """View methods for the Priority Group model.
+  """
+
+  def __init__(self, params=None):
+    """Defines the fields and methods required for the base View class
+    to provide the user with list, public, create, edit and delete views.
+
+    Params:
+      params: a dict with params for this View
+    """
+
+    rights = access.Checker(params)
+
+    new_params = {}
+    new_params['rights'] = rights
+    new_params['logic'] = priority_group_logic
+
+    new_params['name'] = "Priority Group"
+
+    new_params['no_create_raw'] = True
+    new_params['no_create_with_scope'] = True
+    new_params['no_create_with_key_fields'] = True
+
+    params = dicts.merge(params, new_params)
+
+    super(View, self).__init__(params=params)
+
+
+view = View()
+
+delete = decorators.view(view.delete)
+edit = decorators.view(view.edit)
+list = decorators.view(view.list)
+public = decorators.view(view.public)

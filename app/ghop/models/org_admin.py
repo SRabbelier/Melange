25e890e4e350925750c47ee2892a52a266abe2f5
Lennard de Rijk <ljvderijk@gmail.com>
2009-05-30 01:03:20 +0200

    Added GHOP specific Mentor and Org Admin Models.

---
+++ app/ghop/models/org_admin.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2009 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the GHOP specific Org Admin Model.
+"""
+
+__authors__ = [
+  '"Lennard de Rijk" <ljvderijk@gmail.com>',
+]
+
+
+import soc.models.org_admin
+
+
+class GHOPOrgAdmin(soc.models.org_admin.OrgAdmin):
+  """GHOP Org Admin model extends the basic Org Admin model.
+  """
+
+  pass

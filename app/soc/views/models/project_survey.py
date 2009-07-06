089c456e8df62be04f8c875a8cc85aff3710a8ae
Daniel Diniz <ajaksu@gmail.com>
2009-07-06 17:03:19 +0200

    Add missing import to Project Survey view.
    Reviewed by: Lennard de Rijk

--- app/soc/views/models/project_survey.py
+++ app/soc/views/models/project_survey.py
@@ -30,6 +30,7 @@ from soc.views.helper import access
 from soc.views.helper import decorators
 from soc.views.helper import lists
 from soc.views.helper import redirects
+from soc.views.helper import responses
 from soc.views.models import survey
 
 

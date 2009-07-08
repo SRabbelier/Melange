4c475ab8e93023d0ceedcdecf70a70122c3ac7d6
Augie Fackler <durin42@gmail.com>
2009-07-07 20:17:23 -0500

    Start using buildout to bring in external dependencies.
    This makes running tests easier in situations where dependencies are not always available, such as buildbot.
    
    Thanks to Matthew Wilkes <matthew@matthewwilkes.co.uk> for help and moral support.
    
    --HG--
    extra : rebase_source : 397d5e828ed0dbc512dbd7321f26cf210bd65a94

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""Minimal setup script to appease buildout for Melange.
+"""
+import os
+import re
+from setuptools import setup, find_packages
+
+match_version = re.compile("version: ([0-9\-]+)")
+try:
+    appyaml = open(os.path.join("app", "app.yaml.template"))
+    version = match_version.findall(appyaml.read())[0]
+except:
+    version = "UNKNOWN"
+
+
+setup(
+    name = 'melange',
+    description=("The goal of this project is to create a framework for "
+                 "representing Open Source contribution workflows, such as"
+                 " the existing Google Summer of Code TM (GSoC) program."),
+    version = version,
+    packages = find_packages(exclude=['app.django.*','thirdparty','parts']),
+    author=open("AUTHORS").read(),
+    url='http://code.google.com/p/soc',
+    license='Apache2',
+    install_requires = [
+        ],
+    tests_require=[
+        'zope.testbrowser',
+        'gaeftest',
+        'nose',
+        ],
+    entry_points = {'console_scripts': ['run-tests = tests.run:main',
+                                        ],
+                    },
+    include_package_data = True,
+    zip_safe = False,
+    )

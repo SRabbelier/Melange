4eec39351700b15b99d5bf4ce6052a31a4d7e094
Lennard de Rijk <ljvderijk@gmail.com>
2009-08-12 10:17:08 -0700

    Added basic GSoC module models.
    These models will be filled with GSoC specific entries once the transformation is complete.

---
+++ app/soc/modules/gsoc/models/student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2009 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the GSoC specific Student Model.
+"""
+
+__authors__ = [
+  '"Lennard de Rijk" <ljvderijk@gmail.com>',
+]
+
+
+import soc.models.student
+
+
+class GSoCStudent(soc.models.student.Student):
+  """GSoC Student model extends the basic Student model.
+  """
+  pass

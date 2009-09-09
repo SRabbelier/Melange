8f99dff0f2d7f86d34d4bcc3100cc15f447dd113
Lennard de Rijk <ljvderijk@gmail.com>
2009-09-09 21:14:22 +0200

    Added the GHOP modules to the callback.
    The ENABLE_MODULE has been added to ensure that the current build does not suffer from any weird artefacts that might occur due to the presence of this module. Of course in time it will be removed.
    
    Thanks Madhusudan for getting us this far ^_^. Minor CSS and image patches should follow shortly.

--- app/soc/modules/ghop/callback.py
+++ app/soc/modules/ghop/callback.py
@@ -20,12 +20,24 @@ __authors__ = [
   '"Lennard de Rijk" <ljvderijk@gmail.com>',
   ]
 
+from soc.modules.ghop.tasks import task_update
+from soc.modules.ghop.views.models import mentor
+from soc.modules.ghop.views.models import organization
+from soc.modules.ghop.views.models import org_admin
+from soc.modules.ghop.views.models import program
+from soc.modules.ghop.views.models import task
+from soc.modules.ghop.views.models import task_subscription
+from soc.modules.ghop.views.models import timeline
+
 
 class Callback(object):
   """Callback object that handles interaction between the core.
   """
 
   API_VERSION = 1
+
+  # TODO: set this to True if you want to enable the GHOP module
+  ENABLE_MODULE = False
 
   def __init__(self, core):
     """Initializes a new Callback object for the specified core.
@@ -39,9 +51,37 @@ class Callback(object):
 
     self.core.requireUniqueService('registerWithSitemap')
 
+    if not self.ENABLE_MODULE:
+      return
+
+    # register the GHOP Views
+    self.core.registerSitemapEntry(mentor.view.getDjangoURLPatterns())
+    self.core.registerSitemapEntry(organization.view.getDjangoURLPatterns())
+    self.core.registerSitemapEntry(org_admin.view.getDjangoURLPatterns())
+    self.core.registerSitemapEntry(program.view.getDjangoURLPatterns())
+    self.core.registerSitemapEntry(task.view.getDjangoURLPatterns())
+    self.core.registerSitemapEntry(task_subscription.view.getDjangoURLPatterns())
+    self.core.registerSitemapEntry(timeline.view.getDjangoURLPatterns())
+
+    # register GHOP GAE Tasks URL's
+    self.core.registerSitemapEntry(task_update.getDjangoURLPatterns())
+
   def registerWithSidebar(self):
     """Called by the server when sidebar entries should be registered.
     """
 
     # require that we had the chance to register the urls we need with the sitemap
     self.core.requireUniqueService('registerWithSidebar')
+
+    if not self.ENABLE_MODULE:
+      return
+
+    # register the GHOP menu entries
+    self.core.registerSidebarEntry(mentor.view.getSidebarMenus)
+    self.core.registerSidebarEntry(organization.view.getExtraMenus)
+    self.core.registerSidebarEntry(organization.view.getSidebarMenus)
+    self.core.registerSidebarEntry(org_admin.view.getSidebarMenus)
+    self.core.registerSidebarEntry(program.view.getSidebarMenus)
+    self.core.registerSidebarEntry(task.view.getSidebarMenus)
+    self.core.registerSidebarEntry(task_subscription.view.getSidebarMenus)
+    self.core.registerSidebarEntry(timeline.view.getSidebarMenus)

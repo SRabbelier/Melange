d4d7ef69219c020d9755cb38a4dc6709fd1cea6f
Lennard de Rijk <ljvderijk@gmail.com>
2009-12-05 14:04:47 +0100

    Hooked up the module_conversion updates to the start_update module.

--- app/soc/tasks/updates/start_update.py
+++ app/soc/tasks/updates/start_update.py
@@ -28,6 +28,7 @@ from django.template import loader
 from django.utils.translation import ugettext
 
 from soc.tasks.helper import error_handler
+from soc.tasks.updates import module_conversion
 from soc.tasks.updates import student_school_type
 from soc.views.helper import responses
 
@@ -96,6 +97,173 @@ class TaskRunner(object):
       'runner': student_school_type.runSchoolTypeUpdate,
       }
 
+  PROGRAM_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 1,
+      'description': ugettext(
+          'Update that converts Program->GSoCProgram entities for use in the '
+          'new Module system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runProgramConversionUpdate,
+      }
+
+  ORG_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 2,
+      'description': ugettext(
+          'Update that converts Organization->GSoCOrganization entities for '
+          'use in the new Module system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runOrgConversionUpdate,
+      }
+
+  ORG_ADMIN_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 3,
+      'description': ugettext(
+          'Update that converts OrgAdmin->GSoCOrgAdmin entities for '
+          'use in the new Module system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runOrgAdminConversionUpdate,
+      }
+
+  MENTOR_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 4,
+      'description': ugettext(
+          'Update that converts Mentor->GSoCMentor entities for '
+          'use in the new Module system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runMentorConversionUpdate,
+      }
+
+  STUDENT_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 5,
+      'description': ugettext(
+          'Update that converts Student->GSoCStudent entities for '
+          'use in the new Module system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runStudentConversionUpdate,
+      }
+
+  STUDENT_PROPOSAL_MODULE = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 6,
+      'description': ugettext(
+          'Update that sets the properties in the StudentProposal to match '
+          'the new entities in the Module system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runStudentProposalUpdate,
+      }
+
+  REVIEW_MODULE = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 7,
+      'description': ugettext(
+          'Update that removes the reviewer property from the Review entity.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runReviewUpdate,
+      }
+
+  STUDENT_PROJECT_MODULE = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 8,
+      'description': ugettext(
+          'Update that sets the properties in the StudentProject to match '
+          'the new entities in the Module system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runStudentProjectUpdate,
+      }
+
+  SURVEY_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 9,
+      'description': ugettext(
+          'Update that changes prefix from Program to GSoCProgram and updates '
+          'the necessary references for Surveys.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runSurveyUpdate,
+      }
+
+  PROJECT_SURVEY_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 10,
+      'description': ugettext(
+          'Update that changes prefix from Program to GSoCProgram and updates '
+          'the necessary references for ProjectSurveys.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runProjectSurveyUpdate,
+      }
+
+  GRADING_PROJECT_SURVEY_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 11,
+      'description': ugettext(
+          'Update that changes prefix from Program to GSoCProgram and updates '
+          'the necessary references for GradingProjectSurveys.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runGradingProjectSurveyUpdate,
+      }
+
+  SURVEY_RECORD_MODULE = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 12,
+      'description': ugettext(
+          'Update for SurveyRecords to point to the new Surveys'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runSurveyRecordUpdate,
+      }
+
+  PROJECT_SURVEY_RECORD_MODULE = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 13,
+      'description': ugettext(
+          'Update for ProjectSurveyRecords to point to the new Surveys and '
+          'update the Organization reference.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runProjectSurveyRecordUpdate,
+      }
+
+  GRADING_PROJECT_SURVEY_RECORD_MODULE = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 14,
+      'description': ugettext(
+          'Update for GradingProjectSurveyRecords to point to the new Surveys '
+          'and update the Organization reference.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runGradingProjectSurveyRecordUpdate,
+      }
+
+  GRADING_SURVEY_GROUP_MODULE = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 15,
+      'description': ugettext(
+          'Update for GradingSurveyGroups to point to the new Surveys.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runGradingSurveyGroupUpdate,
+      }
+
+  DOCUMENT_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 16,
+      'description': ugettext(
+          'Update to change the prefixes of Documents to the new Module '
+          'system.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runDocumentUpdate,
+      }
+
+  TIMELINE_MODULE_CONVERSION = {
+      'from_version': '0-5-20091121',
+      'in_version_order': 17,
+      'description': ugettext(
+          'Update to convert Timeline->GSoCTimeline and also update the '
+          'Program reference to point to the new GSoCProgram.'),
+      'starter': module_conversion.startUpdateWithUrl,
+      'runner': module_conversion.runTimelineConversionUpdate,
+      }
+
 
   def __init__(self):
     """Initializes the TaskRunner.
@@ -103,6 +271,26 @@ class TaskRunner(object):
 
     self.options = {
         'student_school_type': self.STUDENT_SCHOOL_TYPE,
+        'program_module_conversion': self.PROGRAM_MODULE_CONVERSION,
+        'org_module_conversion': self.ORG_MODULE_CONVERSION,
+        'org_admin_module_conversion': self.ORG_ADMIN_MODULE_CONVERSION,
+        'mentor_module_conversion': self.MENTOR_MODULE_CONVERSION,
+        'student_module_conversion': self.STUDENT_MODULE_CONVERSION,
+        'student_proposal_module': self.STUDENT_PROPOSAL_MODULE,
+        'review_module': self.REVIEW_MODULE,
+        'student_project_module': self.STUDENT_PROJECT_MODULE,
+        'survey_module_conversion': self.SURVEY_MODULE_CONVERSION,
+        'project_survey_module_conversion': 
+            self.PROJECT_SURVEY_MODULE_CONVERSION,
+        'grading_project_survey_module_conversion': 
+            self.GRADING_PROJECT_SURVEY_MODULE_CONVERSION,
+        'survey_record_module': self.SURVEY_RECORD_MODULE,
+        'project_survey_record_module': self.PROJECT_SURVEY_RECORD_MODULE,
+        'grading_project_survey_record_module':
+            self.GRADING_PROJECT_SURVEY_RECORD_MODULE,
+        'grading_survey_group_module': self.GRADING_SURVEY_GROUP_MODULE,
+        'document_module_conversion': self.DOCUMENT_MODULE_CONVERSION,
+        'timeline_module_conversion': self.TIMELINE_MODULE_CONVERSION,
     }
 
   def getOptions(self):

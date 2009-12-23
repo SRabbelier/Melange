bc653291c3b61f2e1d11d26109dacbb4bed8dfe2
Daniel Hans <Daniel.M.Hans@gmail.com>
2009-12-23 19:47:36 +0100

    Sidebar submenus may be collapsed at the time when the page is loaded.
    Such collapsed lists should have 'expandable-collapsed' class.

--- app/soc/content/js/menu-090825.js
+++ app/soc/content/js/menu-090825.js
@@ -1,12 +1,12 @@
 jQuery(function () {
   var new_item_text = "(new)";
-  jQuery('#menu li.expandable').find('a').each(function () {
+  jQuery('#menu li.expandable, #menu li.expandable-collapsed').find('a').each(function () {
     if (jQuery(this).text().indexOf(new_item_text) > -1) {
       jQuery(this).css('color', 'red');
     }
   });
 
-  jQuery('#side #menu li.expandable > a').dblclick(function () {
+  jQuery('#side #menu li.expandable > a, #side #menu li.expandable-collapsed > a').dblclick(function () {
     window.location = jQuery(this).attr('href');
   });
 
@@ -19,6 +19,20 @@ jQuery(function () {
     return false;
   });
 
+  jQuery('#side #menu li.expandable-collapsed > span').toggle(function () {
+	    jQuery(this).find("img").attr('src', '/soc/content/images/minus.gif')
+	    .end().parent().children("ul").toggle();
+	  }, function () {
+	    jQuery(this).find("img").attr('src', '/soc/content/images/plus.gif')
+	    .end().parent().children("ul").toggle();
+	    return false;
+	  });
+
   jQuery('#side #menu li.expandable > span').contents()
   .before('<img src="/soc/content/images/minus.gif" />');
+
+  jQuery('#side #menu li.expandable-collapsed > span').contents()
+  .before('<img src="/soc/content/images/plus.gif" />');
+
+  jQuery('#side #menu li.expandable-collapsed > ul').toggle();
 });

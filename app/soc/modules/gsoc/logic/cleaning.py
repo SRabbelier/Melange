367e59c8fdd845d16677b11c2eb4f2f16ae31d19
Daniel Hans <Daniel.M.Hans@gmail.com>
2010-01-05 17:00:42 -0500

    Added cleaning functions for gsoc module.
    This should be added in the previous commit.

---
+++ app/soc/modules/gsoc/logic/cleaning.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2010 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""GSOC module cleaning methods.
+"""
+
+__authors__ = [
+    '"Daniel Hans" <daniel.m.hans@gmail.com>',
+    ]
+
+
+import re
+
+from django import forms
+from django.utils.translation import ugettext
+
+from soc.logic import cleaning
+
+
+def cleanTagsList(field_name):
+  """Clean method to check and validate list of tag values.
+  """
+
+  def wrapper(self):
+    """Decorator wrapped method.
+    """
+
+    tag_values = cleaning.str2set(field_name, '\n')(self)
+
+    for tag_value in tag_values:
+
+      if not re.search('\A[a-z0-9_#+\-\.]*\Z', tag_value):
+        raise forms.ValidationError(ugettext(
+            ('%s is not a valid tag value. Please see help for more details.'
+            % tag_value)))
+
+    return tag_values
+  return wrapper

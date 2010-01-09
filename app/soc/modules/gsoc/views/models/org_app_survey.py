18f04b000c3a75b1cce4ad2747eb63417c49a6fc
Lennard de Rijk <ljvderijk@gmail.com>
2010-01-09 04:12:59 +0100

    Added OrgAppSurvey View to the GSoC and GHOP module.

---
+++ app/soc/modules/gsoc/views/models/org_app_survey.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2010 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Views for GSoCOrgAppSurveys.
+"""
+
+
+__authors__ = [
+  '"Lennard de Rijk" <ljvderijk@gmail.com>',
+  ]
+
+
+from soc.logic import dicts
+from soc.views.helper import decorators
+from soc.views.models import org_app_survey
+
+from soc.modules.gsoc.logic.models.org_app_survey import logic as org_app_logic
+from soc.modules.gsoc.logic.models.program import logic as program_logic
+from soc.modules.gsoc.logic.models.student import logic as student_logic
+from soc.modules.gsoc.views.helper import access
+from soc.modules.gsoc.views.models.program import view as program_view
+
+
+class View(org_app_survey.View):
+  """View methods for the GSoCOrgAppSurveys model.
+  """
+
+  def __init__(self, params=None):
+    """Defines the fields and methods required for the base View class
+    to provide the user with list, public, create, edit and delete views.
+
+    Params:
+      params: a dict with params for this View
+    """
+
+    rights = access.GSoCChecker(params)
+    rights['any_access'] = ['allow']
+    rights['show'] = ['checkIsDeveloper']
+    rights['create'] = [('checkIsHostForProgramInScope', program_logic)]
+    rights['edit'] = [('checkIsHostForProgramInScope', program_logic)]
+    rights['delete'] = ['checkIsDeveloper']
+    rights['list'] = ['checkIsDeveloper']
+    rights['list_self'] = ['checkIsUser']
+    rights['record'] = [('checkHasAny', [
+        [('checkIsMyOrgAppRecord', []),
+        ('checkIsSurveyReadable', [org_app_logic])]
+        ])]
+    rights['results'] = [('checkIsHostForProgramInScope', program_logic)]
+    rights['take'] = [
+        'checkOrgAppRecordIfPresent',
+        ('checkIsActivePeriod',
+            ['org_signup', 'scope_path', program_logic]),
+        ('checkIsSurveyTakeable', org_app_logic),
+        ('checkIsNotStudentForProgramInScope', [program_logic, student_logic])]
+
+    new_params = {}
+    new_params['logic'] = org_app_logic
+    new_params['rights'] = rights
+
+    new_params['scope_view'] = program_view
+
+    new_params['name'] = "GSoC Org Application Survey"
+    new_params['url_name'] = 'gsoc/org_app'
+    new_params['module_package'] = 'soc.modules.gsoc.views.models'
+    new_params['module_name'] = 'org_app_survey'
+
+    params = dicts.merge(params, new_params, sub_merge=True)
+
+    super(View, self).__init__(params=params)
+
+
+view = View()
+
+create = decorators.view(view.create)
+edit = decorators.view(view.edit)
+delete = decorators.view(view.delete)
+list = decorators.view(view.list)
+list_self = decorators.view(view.list_self)
+public = decorators.view(view.public)
+record = decorators.view(view.viewRecord)
+results = decorators.view(view.viewResults)
+take = decorators.view(view.take)

75d592609d9ff9e5b0059e9ed5174a6cde7ea67a
Sverre Rabbelier <srabbelier@gmail.com>
2010-02-06 17:47:27 +0100

    Remove duplicate survey fields
    These fields are already specified in soc.views.models.survey.

--- app/soc/modules/gsoc/views/models/project_survey.py
+++ app/soc/modules/gsoc/views/models/project_survey.py
@@ -92,19 +92,6 @@ class View(survey.View):
     new_params['manage_student_project_row'] = \
         'soc/project_survey/list/row_manage_student_project.html'
 
-    new_params['public_field_extra'] = lambda entity: {
-        "path": entity.scope_path + '/' + entity.link_id ,
-        "created_by": entity.author.link_id,
-    }
-    new_params['public_field_keys'] = [
-        "path", "title", "link_id", "is_featured",
-        "created_by", "created", "modified",
-    ]
-    new_params['public_field_names'] = [
-        "Path", "Title", "Link ID", "Featured",
-        "Created By", "Created On", "Modified",
-    ]
-
     params = dicts.merge(params, new_params, sub_merge=True)
 
     super(View, self).__init__(params=params)

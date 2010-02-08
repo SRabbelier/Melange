50c7f2c17d408307a1b8bdbf939455470e1538ab
Mario Ferraro <fadinlight@gmail.com>
2010-02-08 15:08:26 +0100

    Externalize autocomplete code to a JS package.

---
+++ app/soc/content/js/melange.autocomplete-100204.js
@@ -0,0 +1,59 @@
+/* Copyright 2010 the Melange authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @author <a href="mailto:fadinlight@gmail.com">Mario Ferraro</a>
+ */
+(function () {
+  /** @lends melange.autocomplete */
+
+  if (window.melange === undefined) {
+    throw new Error("Melange not loaded");
+  }
+
+  var melange = window.melange;
+
+  /** Package that handles all tooltips related functions
+    * @name melange.autocomplete
+    * @namespace melange.autocomplete
+    * @borrows melange.logging.debugDecorator.log as log
+    */
+  melange.autocomplete = window.melange.autocomplete = function () {
+    return new melange.autocomplete();
+  };
+
+  /** Shortcut to current package.
+    * @private
+    */
+  var $m = melange.logging.debugDecorator(melange.autocomplete);
+
+  melange.error.createErrors([
+  ]);
+
+  $m.makeAutoComplete = function (id, url) {
+    jQuery.getJSON(url,
+      function(data){
+        $("#" + id).autocomplete(data.data, {
+          matchContains: true,
+          formatItem: function(item) {
+            return item.link_id+" ("+item.title+")";
+          },
+          formatResult: function(item) {
+            return item.link_id;
+          }
+        });
+      }
+    );
+  }
+}());

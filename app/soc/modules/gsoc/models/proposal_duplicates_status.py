ce87b43f4b43d34d7c96a552285629e00ed00120
Madhusudan.C.S <madhusudancs@gmail.com>
2010-02-26 16:01:25 +0100

    Added/Edited models for the new duplicates finding system.
    Reviewed by: Lennard de Rijk

--- app/soc/modules/gsoc/models/proposal_duplicates_status.py
+++ app/soc/modules/gsoc/models/proposal_duplicates_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.5
 #
-# Copyright 2009 the Melange authors.
+# Copyright 2010 the Melange authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""This module contains the ProposalDuplicates Model.
+"""This module contains the ProposalDuplicatesStatus Model.
 """
 
 __authors__ = [
   '"Madhusudan.C.S" <madhusudancs@gmail.com>',
-  '"Lennard de Rijk" <ljvderijk@gmail.com>',
 ]
 
 
@@ -28,26 +27,22 @@ from google.appengine.ext import db
 import soc.models.base
 
 import soc.modules.gsoc.models.program
-import soc.modules.gsoc.models.student
 
 
-class ProposalDuplicate(soc.models.base.ModelWithFieldAttributes):
-  """Model used to store the duplicate proposals for a student
-     in a Project-Based Program.
+class ProposalDuplicatesStatus(soc.models.base.ModelWithFieldAttributes):
+  """Model used to store the status of the duplicate proposals stored
+  in the corresponding ProposalDuplicate model.
   """
 
   #: Program in which this duplicates exist
   program = db.ReferenceProperty(
       reference_class=soc.modules.gsoc.models.program.GSoCProgram,
-      required=True, collection_name='duplicate_proposals')
+      required=True, collection_name='duplicates_status')
 
-  #: Student who has these duplicates
-  student = db.ReferenceProperty(
-      reference_class=soc.modules.gsoc.models.student.GSoCStudent,
-      required=True, collection_name='students_duplicates')
+  #: Status of the duplicate proposals. When the calculation is
+  #: happening the status will be 'processing', 'idle' otherwise
+  status = db.StringProperty(required=True, choices=['processing', 'idle'],
+                             default='idle')
 
-  #: List of organizations to which the proposals belong to
-  orgs = db.ListProperty(item_type=db.Key, default=[])
-
-  #: List of all proposals that would be accepted for this student
-  duplicates = db.ListProperty(item_type=db.Key, default=[])
+  #: date when this calculation was last updated
+  calculated_on = db.DateTimeProperty()

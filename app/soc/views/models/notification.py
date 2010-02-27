53db090cf517fdde2d96ecde8b5f8a398788d6c0
Lennard de Rijk <ljvderijk@gmail.com>
2010-02-27 15:28:55 +0100

    Fixed notification list using load instead of loads to load the JSON string.

--- app/soc/views/models/notification.py
+++ app/soc/views/models/notification.py
@@ -198,7 +198,7 @@ class View(base.View):
 
     post_dict = request.POST
 
-    data = simplejson.load(post_dict.get('data', '[]'))
+    data = simplejson.loads(post_dict.get('data', '[]'))
     button_id = post_dict.get('button_id', '')
 
     user_entity = user_logic.getForCurrentAccount()

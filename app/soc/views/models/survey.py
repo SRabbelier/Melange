d976cdc09c25868c5d75e782d8428a975acd43d0
Lennard de Rijk <ljvderijk@gmail.com>
2010-03-06 01:03:06 +0100

    Use document prefix to query for surveys to be shown in a Program menu.
    --HG--
    extra : rebase_source : df4237c01c891225f9052470d8dbd479c45aca17

--- app/soc/views/models/survey.py
+++ app/soc/views/models/survey.py
@@ -916,7 +916,7 @@ class View(base.View):
 
     # filter all featured surveys for the given entity
     filter = {
-        'prefix' : params['url_name'],
+        'prefix' : params['document_prefix'],
         'scope_path': entity.key().id_or_name(),
         'is_featured': True,
         }

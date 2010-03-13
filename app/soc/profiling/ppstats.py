9b36de07a8a93c1bf9b8be7d8d37c5fd8e6b3e00
Sverre Rabbelier <srabbelier@gmail.com>
2010-03-13 23:06:30 +0100

    Add license and authors to ppstats module

--- app/soc/profiling/ppstats.py
+++ app/soc/profiling/ppstats.py
@@ -1,3 +1,30 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2010 the Melange authors.
+# Copyright 2009 Jake McGuire.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module containing a custom Stats object for profiling.
+"""
+
+__authors__ = [
+  '"Jake McGuire" <jaekmcguire@gmail.com>',
+  '"Sverre Rabbelier" <sverre@rabbelier.nl>',
+  ]
+
+
+
 import pstats
 import cPickle
 import zlib

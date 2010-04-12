ab85db461e34bae17b7510204bc405736af26e05
Madhusudan.C.S <madhusudancs@gmail.com>
2010-04-12 21:18:39 +0530

    Fix the date picker to show the year till the current year plus one.
    --HG--
    extra : rebase_source : 251155a2af486272154e6eb5e993859fc4fd0650

--- app/soc/content/js/datetimepicker-090825.js
+++ app/soc/content/js/datetimepicker-090825.js
@@ -1,11 +1,12 @@
 jQuery(
   function () {
+	var cur_year = new Date().getFullYear();
     jQuery('.datetime-pick').datetimepicker();
     jQuery('.date-pick').datetimepicker({
       'pickDateOnly': true,
       'defaultDate': new Date('01/01/1974'),
       'timeFormat': '',
-      'yearRange': '1900:2008'
+      'yearRange': '1900:' + (cur_year + 1)
     });
   }
 );

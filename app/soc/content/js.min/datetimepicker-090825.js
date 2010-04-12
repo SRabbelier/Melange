5456f37d8c5ed38edfddbbc21ffd787c1343fa78
Sverre Rabbelier <srabbelier@gmail.com>
2010-04-12 21:43:43 +0200

    Commit minimized version of datapicker and allocation

--- app/soc/content/js.min/datetimepicker-090825.js
+++ app/soc/content/js.min/datetimepicker-090825.js
@@ -1,1 +1,1 @@
-jQuery(function(){jQuery(".datetime-pick").datetimepicker();jQuery(".date-pick").datetimepicker({pickDateOnly:true,defaultDate:new Date("01/01/1974"),timeFormat:"",yearRange:"1900:2008"})});
+jQuery(function(){var a=(new Date).getFullYear();jQuery(".datetime-pick").datetimepicker();jQuery(".date-pick").datetimepicker({pickDateOnly:true,defaultDate:new Date("01/01/1974"),timeFormat:"",yearRange:"1900:"+(a+1)})});

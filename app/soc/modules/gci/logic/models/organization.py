292982988e05c8aee4baa70e57ca77e11fcd6d31
Madhusudan.C.S <madhusudancs@gmail.com>
2010-09-12 16:04:20 +0530

    Add a new logic method to compute the remaining quota for the organization.

--- app/soc/modules/gci/logic/models/organization.py
+++ app/soc/modules/gci/logic/models/organization.py
@@ -44,5 +44,34 @@ class Logic(organization.Logic):
     super(Logic, self).__init__(model, base_model=base_model,
                                 scope_logic=scope_logic)
 
+  def getRemainingTaskQuota(self, entity):
+    """Returns the number of remaining tasks that the organization can publish.
+
+    While calculating the remaining quota we consider all the tasks that
+    were published including the closed tasks but not the deleted tasks.
+
+    Args:
+      entity: The organization entity for which the quota must be calculated
+
+    Returns:
+      An integer which is the number of tasks the organization can publish yet
+    """
+
+    from soc.modules.gci.logic.models import task as gci_task_logic
+
+    # TODO(Madhu): Refactor to create Open Tasks and Closed tasks variables
+    # count all the tasks the organization has published till now.
+    # This excludes tasks in Unapproved, Unpublished and Invalid states.
+    fields = {
+        'scope': entity,
+        'status': ['Open', 'Reopened', 'ClaimRequested', 'Claimed',
+                   'ActionNeeded', 'Closed', 'AwaitingRegistration',
+                   'NeedsWork', 'NeedsReview']
+        }
+
+    task_query = gci_task_logic.logic.getQueryForFields(fields)
+
+    return entity.task_quota_limit - task_query.count()
+
 
 logic = Logic()

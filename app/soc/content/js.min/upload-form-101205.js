992cc3318e2fe08f7a53a1962730360d3675afde
Sverre Rabbelier <srabbelier@gmail.com>
2010-12-05 03:19:18 +0100

    Add minified version of upload-form.js

---
+++ app/soc/content/js.min/upload-form-101205.js
@@ -0,0 +1,1 @@
+(function(){if(window.melange===undefined)throw new Error("Melange not loaded");window.melange.getUploadUrl=function(){var a="?";if(window.location.href.indexOf("?")!==-1)a="&";return jQuery.ajax({async:false,cache:false,url:[window.location.href,a,"fmt=json"].join(""),error:function(){alert("Could not retrieve upload url")}}).responseText}})(jQuery);

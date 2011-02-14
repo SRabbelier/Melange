f600c7f97e32ddb33adc4506c69b2e67ad37e7f7
Madhusudan.C.S <madhusudancs@gmail.com>
2011-02-15 02:27:43 +0530

    Inherit HTTP request handlers for GSoC specific views from the base module.
    At the moment we only add an object of RequestData class to the
    RequestHandler.
    
    --HG--
    extra : rebase_source : 5bbef181e7d631abd01a456194cdaf592ceca385

---
+++ app/soc/modules/gsoc/views/base.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module containing the boiler plate required to construct GSoC views.
+"""
+
+__authors__ = [
+  '"Madhusudan.C.S" <madhusudancs@gmail.com>',
+  ]
+
+
+from soc.views.base import RequestHandler
+
+from soc.modules.gsoc.views.helper.request_data import RequestData
+
+
+class RequestHandler(RequestHandler):
+  """Customization required by GSoC to handle HTTP requests.
+  """
+
+  def __call__(self, request, *args, **kwargs):
+    """See soc.views.base.RequestHandler.__call__()
+    """
+
+    self.data = RequestData(request, *args, **kwargs)
+
+    return super(RequestHandler, self).__call__(request, *args, **kwargs)

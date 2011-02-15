6ac2aa3a9e31153ea7553cae5890118d60db5fca
Lennard de Rijk <ljvderijk@gmail.com>
2011-02-15 14:38:32 +0000

    Added first version of module for GSoC url patterns.
    We have chosen for more concrete naming of the fields in the pattern. A selection of patterns has already been included.

---
+++ app/soc/modules/gsoc/views/helper/url_patterns.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module for constructing GSoC related URL patterns
+"""
+
+__authors__ = [
+  '"Lennard de Rijk" <ljvderijk@gmail.com>',
+  ]
+
+
+from soc.models import linkable
+
+
+def namedLinkIdPattern(names):
+  """Returns a link ID pattern consisting of named parts.
+
+  The returned pattern does not start or end with a /, the parts are however
+  concatenated with a /.
+
+  Args:
+    names: The names that should be given to the different parts.
+  """
+  named_patterns = []
+  for name in names:
+    named_patterns.append(r'(?P<%s>%s)' % (name, linkable.LINK_ID_PATTERN_CORE))
+
+  return r'/'.join(named_patterns)
+
+PROGRAM_PATTERN = namedLinkIdPattern(['sponsor', 'program'])
+STUDENT_PATTERN = namedLinkIdPattern(['sponsor', 'program', 'student'])
+MENTOR_PATTERN = namedLinkIdPattern(['sponsor', 'program', 'organization', 'mentor'])
+ORG_ADMIN_PATTERN = namedLinkIdPattern(['sponsor', 'program', 'organization', 'org_admin'])
+PROPOSAL_PATTERN = namedLinkIdPattern(['sponsor', 'program', 'student', 'proposal'])
+PROJECT_PATTERN = namedLinkIdPattern(['sponsor', 'program', 'organization', 'project'])

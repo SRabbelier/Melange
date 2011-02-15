93c26e95d02d7defa7aaee9af9d3a22ac396fd24
Lennard de Rijk <ljvderijk@gmail.com>
2011-02-15 11:24:44 +0000

    Refactored sidebar to site_menu and scoped it's context.
    Site menu should cover both the main menu and the footer menu since they are similair in their needs.

--- app/soc/modules/gsoc/views/site_menu.py
+++ app/soc/modules/gsoc/views/site_menu.py
@@ -14,28 +14,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""This module contains sidebar view."""
+"""This module contains the view for the site menus."""
 
 __authors__ = [
   '"Daniel Hans" <daniel.m.hans@gmail.com>',
+  '"Lennard de Rijk" <ljvderijk@gmail.com>',
 ]
 
 
 from google.appengine.api import users
 
 from soc.modules.gsoc.views.helper import redirects
- 
+
 
 # TODO: this is a draft version of the class. It will be changed due to
 # some further decisions on how the views should look like
-class Sidebar(object):
-  """Sidebar view.
+class SiteMenu(object):
+  """SiteMenu view.
   """
 
-  def getSidearContext(self, request):
-    """Generates URL links for the hard-coded GSoC sidebar items.
+  def getContext(self, request):
+    """Generates URL links for the hard-coded GSoC site menu items.
     """
-
     context = {}
 
     # get login link
@@ -61,4 +61,4 @@ class Sidebar(object):
     # get help link
     context['help'] = redirects.getHelpRedirect()
 
-    return context
+    return {'site_menu': context}

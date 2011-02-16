e40f34dd07e147db1c6e1e88b3279d1da0ae68e5
Lennard de Rijk <ljvderijk@gmail.com>
2011-02-16 18:25:28 +0000

    Updated dashboard with a MyProjectsComponent.
    --HG--
    extra : rebase_source : 58e36b3d10494bfbf7c51520edbbc4d2888e8961

--- app/soc/modules/gsoc/views/dashboard.py
+++ app/soc/modules/gsoc/views/dashboard.py
@@ -23,6 +23,7 @@ __authors__ = [
 
 
 from soc.logic.lists import Lists
+from soc.views import out_of_band
 from soc.views.helper import lists
 from soc.views.template import Template
 
@@ -30,7 +31,10 @@ from soc.modules.gsoc.logic.models.student_project import logic as \
     project_logic
 from soc.modules.gsoc.views.base import RequestHandler
 from soc.modules.gsoc.views.helper import url_patterns
-
+from soc.modules.gsoc.views.models.student_proposal import view as \
+    proposal_view
+from soc.modules.gsoc.views.models.student_project import view as \
+    project_view
 
 class Dashboard(RequestHandler):
   """View for the participant dashboard.
@@ -39,7 +43,7 @@ class Dashboard(RequestHandler):
   def djangoURLPatterns(self):
     """The URL pattern for the dashboard.
     """
-    return [(r'^gsoc/dashboard/%s$' % url_patterns.PROGRAM, self)]
+    return [(r'^gsoc/dashboard/%s$' %url_patterns.PROGRAM, self)]
 
   def checkAccess(self):
     """Denies access if you don't have a role in the current program.
@@ -55,34 +59,57 @@ class Dashboard(RequestHandler):
   def context(self):
     """Handler for default HTTP GET request.
     """
-    components = []
-    if self.data.student:
-      components = self._getStudentComponents()
-    elif self.data.mentors or self.data.org_admins:
-      components = self._getOrgMemberComponents()
+    components = self._getActiveComponents()
 
     context = {}
     context['page_name'] = self.data.program.name
     context['user'] = self.data.user
-    context['alert_msg'] = 'Default Messsage goes here'
+    context['alert_msg'] = 'Default <strong>alert</strong> goes here'
     context['components'] = components
 
     return context
+
+  def _getActiveComponents(self):
+    """Returns the components that are active on the page.
+    """
+    if self.data.student:
+      return self._getStudentComponents()
+    elif self.data.mentors or self.data.org_admins:
+      return self._getOrgMemberComponents()
+    else:
+      return []
+
+  def get(self):
+    """Handler for the GET request.
+    """
+    if lists.isDataRequest(self.request):
+      components = self._getActiveComponents()
+
+      list_data = None
+      for component in components:
+        list_data = component.getListData()
+        if list_data:
+          break
+
+      if not list_data:
+        raise out_of_band.AccessViolation("You do not have access to this data")
+      self.response = lists.getResponse(self.request, list_data)
+    else:
+      super(Dashboard, self).get()
 
   def _getStudentComponents(self):
     """Get the dashboard components for a student.
     """
     # Add all the proposals of this current user
-    components = [MyProposalsComponent(self)]
+    components = [MyProposalsComponent(self.request, self.data)]
 
     fields = {'student': self.data.student}
     project = project_logic.getForFields(fields, unique=True)
     if project:
       # Add a component to show all the projects
-      #MyProjectsComponent()
+      components.append(MyProjectsComponent(self.request, self.data))
       # Add a component to show the evaluations
       #MyEvaluationsComponent()
-      pass
 
     return components
 
@@ -94,29 +121,97 @@ class Dashboard(RequestHandler):
     pass
 
 
-class MyProposalsComponent(Template):
-  """Component for listing all the proposals of the current Student.
+class Component(Template):
+  """Base component for the dashboard.
   """
 
-  def __init__(self, handler):
-    """Initializes the template location, name and title of this component.
+  def __init__(self, request, data):
+    """Initializes the component.
+
+    Args:
+      request: The HTTPRequest object
+      data: The RequestData object
     """
-    self._handler = handler
+    self.request = request
+    self.data = data
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    # by default no list is present
+    return None
+
+
+class MyProposalsComponent(Component):
+  """Component for listing all the proposals of the current Student.
+  """
 
   def templatePath(self):
     """Returns the path to the template that should be used in render().
     """
-    return'v2/modules/gsoc/dashboard/proposals_component.html'
+    return'v2/modules/gsoc/dashboard/list_component.html'
 
   def context(self):
     """Returns the context of this component.
     """
-    from soc.modules.gsoc.views.models.student_proposal import view
-
-    list_config = lists.getListGenerator(self._handler.request,
-                                         view.getParams())
+    list_config = lists.getListGenerator(self.request,
+                                         proposal_view.getParams(), idx=0)
     return {
-        'name': "proposals",
-        'title': "PROPOSALS",
+        'name': 'proposals',
+        'title': 'PROPOSALS',
+        'lists': Lists([list_config]),
+        }
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    idx = lists.getListIndex(self.request)
+    if idx == 0:
+      list_data = lists.getListData(self.request, proposal_view.getParams(),
+                                   {'program': self.data.program,
+                                    'scope': self.data.student})
+      return list_data
+    else:
+      return None
+
+
+class MyProjectsComponent(Component):
+  """Component for listing all the projects of the current Student.
+  """
+
+  def templatePath(self):
+    """Returns the path to the template that should be used in render().
+    """
+    return'v2/modules/gsoc/dashboard/list_component.html'
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    idx = lists.getListIndex(self.request)
+    if idx == 1:
+      list_data = lists.getListData(self.request, project_view.getParams(),
+                                   {'program': self.data.program,
+                                    'student': self.data.student})
+      return list_data
+    else:
+      return None
+
+  def context(self):
+    """Returns the context of this component.
+    """
+    list_config = lists.getListGenerator(self.request,
+                                         project_view.getParams(), idx=1)
+    return {
+        'name': 'projects',
+        'title': 'PROJECTS',
         'lists': Lists([list_config]),
     }

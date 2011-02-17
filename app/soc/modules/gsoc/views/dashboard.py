e8b5cb3a79a030b9f617e365a4b1579490d76b13
Lennard de Rijk <ljvderijk@gmail.com>
2011-02-17 16:11:06 +0000

    Updated dashboard with more components for Students, org members and users.

--- app/soc/modules/gsoc/views/dashboard.py
+++ app/soc/modules/gsoc/views/dashboard.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from soc.modules.gsoc.views.helper.access_checker import AccessChecker
 
 """Module for the GSoC participant dashboard.
 """
@@ -23,19 +22,29 @@ __authors__ = [
   ]
 
 
+from soc.logic.helper import timeline as timeline_helper
 from soc.logic.lists import Lists
 from soc.views import out_of_band
 from soc.views.helper import lists
 from soc.views.template import Template
 
+from soc.modules.gsoc.logic.models.org_app_survey import logic as \
+    org_app_logic
 from soc.modules.gsoc.logic.models.student_project import logic as \
     project_logic
 from soc.modules.gsoc.views.base import RequestHandler
 from soc.modules.gsoc.views.helper import url_patterns
+from soc.modules.gsoc.views.models.organization import view as \
+    org_view
 from soc.modules.gsoc.views.models.student_proposal import view as \
     proposal_view
 from soc.modules.gsoc.views.models.student_project import view as \
     project_view
+from soc.modules.gsoc.views.models.org_app_survey import view as \
+    org_app_view
+from soc.modules.gsoc.views.models.project_survey import view as \
+    project_survey_view
+
 
 class Dashboard(RequestHandler):
   """View for the participant dashboard.
@@ -49,35 +58,16 @@ class Dashboard(RequestHandler):
   def checkAccess(self):
     """Denies access if you don't have a role in the current program.
     """
-    AccessChecker(self.data).checkIsParticipatingInProgram()
+    # TODO(ljvderijk): Would be nice if it would directly redirect you to login
+    # if you are not logged in.
+    # TODO(ljvderijk): You need a "profile" to get access but that needs to be
+    # implemented.
+    pass
 
   def templatePath(self):
     """Returns the path to the template.
     """
     return 'v2/modules/gsoc/dashboard/base.html'
-
-  def context(self):
-    """Handler for default HTTP GET request.
-    """
-    components = self._getActiveComponents()
-
-    context = {}
-    context['page_name'] = self.data.program.name
-    context['user'] = self.data.user
-    context['alert_msg'] = 'Default <strong>alert</strong> goes here'
-    context['components'] = components
-
-    return context
-
-  def _getActiveComponents(self):
-    """Returns the components that are active on the page.
-    """
-    if self.data.student:
-      return self._getStudentComponents()
-    elif self.data.mentors or self.data.org_admins:
-      return self._getOrgMemberComponents()
-    else:
-      return []
 
   def get(self):
     """Handler for the GET request.
@@ -92,10 +82,35 @@ class Dashboard(RequestHandler):
           break
 
       if not list_data:
-        raise out_of_band.AccessViolation("You do not have access to this data")
+        raise out_of_band.AccessViolation(
+            'You do not have access to this data')
       self.response = lists.getResponse(self.request, list_data)
     else:
       super(Dashboard, self).get()
+
+  def context(self):
+    """Handler for default HTTP GET request.
+    """
+    components = self._getActiveComponents()
+
+    context = {}
+    context['page_name'] = self.data.program.name
+    context['user'] = self.data.user
+    # TODO(ljvderijk): Implement code for setting dashboard messages.
+    #context['alert_msg'] = 'Default <strong>alert</strong> goes here'
+    context['components'] = components
+
+    return context
+
+  def _getActiveComponents(self):
+    """Returns the components that are active on the page.
+    """
+    if self.data.student:
+      return self._getStudentComponents()
+    elif self.data.mentors or self.data.org_admins:
+      return self._getOrgMemberComponents()
+    else:
+      return self._getLoneUserCompontents()
 
   def _getStudentComponents(self):
     """Get the dashboard components for a student.
@@ -109,17 +124,52 @@ class Dashboard(RequestHandler):
       # Add a component to show all the projects
       components.append(MyProjectsComponent(self.request, self.data))
       # Add a component to show the evaluations
-      #MyEvaluationsComponent()
+      # TODO(ljvderijk): Enable after the right information can be displayed
+      #components.append(MyEvaluationsComponent(self.request, self.data))
 
     return components
 
   def _getOrgMemberComponents(self):
     """Get the dashboard components for Organization members.
     """
-    # Figure out which orgs are involved
-    #org_roles = self.data.mentors + self.data.org_admins
-    pass
+    components = []
 
+    if self.data.mentors:
+      if timeline_helper.isAfterEvent(
+          self.data.program_timeline, 'accepted_students_announced_deadline'):
+        # add a component to show all projects a user is mentoring
+        components.append(
+            ProjectsIMentorComponent(self.request, self.data))
+
+    if self.data.org_admins:
+      # add a component for all organization that this user administers
+      components.append(OrganizationsIAdminComponent(self.request, self.data))
+
+    if timeline_helper.isBeforeEvent(
+      self.data.program_timeline, 'student_signup_start'):
+      # Add the submitted proposals component
+      #components.append(
+      #    SubmittedProposalsComponent(self.request, self.data))
+      pass
+
+    return components
+
+  def _getLoneUserComponents(self):
+    """Get the dashboard components for users without any role.
+    """
+    components = []
+
+    org_app_survey = org_app_logic.getForProgram(self.data.program)
+
+    fields = {'survey': org_app_survey}
+    org_app_record = org_app_logic.getRecordLogic().getForFields(fields, unique=True)
+
+    if org_app_record:
+      # add a component showing the organization application of the user
+      components.append(MyOrgApplicationsComponent(self.request, self.data,
+                                                   org_app_survey))
+
+    return components
 
 class Component(Template):
   """Base component for the dashboard.
@@ -145,6 +195,58 @@ class Component(Template):
     return None
 
 
+class MyOrgApplicationsComponent(Component):
+  """Component for listing the Organization Applications of the current user.
+  """
+
+  def __init__(self, request, data, org_app_survey):
+    """Initializes the component.
+
+    Args:
+      request: The HTTPRequest object
+      data: The RequestData object
+      org_app_survey: the OrgApplicationSurvey entity
+    """
+    # passed in so we don't have to do double queries
+    self.org_app_survey = org_app_survey
+    super(MyOrgApplicationsComponent, self).__init__(request, data)
+
+  def templatePath(self):
+    """Returns the path to the template that should be used in render().
+    """
+    return'v2/modules/gsoc/dashboard/list_component.html'
+
+  def context(self):
+    """Returns the context of this component.
+    """
+    # TODO(ljvderijk): List redirects
+    list_params = org_app_view.getParams()['record_list_params']
+    list_params['list_description'] = 'List of your Organization Applications'
+    list_config = lists.getListGenerator(self.request, list_params,
+                                         visibility='self', idx=0)
+    return {
+        'name': 'org_applications',
+        'title': 'MY ORGANIZATION APPLICATIONS',
+        'lists': Lists([list_config]),
+        }
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    idx = lists.getListIndex(self.request)
+    if idx == 0:
+      list_data = lists.getListData(
+          self.request, org_app_view.getParams()['record_list_params'],
+          {'survey': self.org_app_survey, 'main_admin': self.data.user},
+          visibility='self')
+      return list_data
+    else:
+      return None
+
+
 class MyProposalsComponent(Component):
   """Component for listing all the proposals of the current Student.
   """
@@ -158,7 +260,7 @@ class MyProposalsComponent(Component):
     """Returns the context of this component.
     """
     list_config = lists.getListGenerator(self.request,
-                                         proposal_view.getParams(), idx=0)
+                                         proposal_view.getParams(), idx=1)
     return {
         'name': 'proposals',
         'title': 'PROPOSALS',
@@ -172,7 +274,7 @@ class MyProposalsComponent(Component):
     returned.
     """
     idx = lists.getListIndex(self.request)
-    if idx == 0:
+    if idx == 1:
       list_data = lists.getListData(self.request, proposal_view.getParams(),
                                    {'program': self.data.program,
                                     'scope': self.data.student})
@@ -197,7 +299,7 @@ class MyProjectsComponent(Component):
     returned.
     """
     idx = lists.getListIndex(self.request)
-    if idx == 1:
+    if idx == 2:
       list_data = lists.getListData(self.request, project_view.getParams(),
                                    {'program': self.data.program,
                                     'student': self.data.student})
@@ -209,9 +311,132 @@ class MyProjectsComponent(Component):
     """Returns the context of this component.
     """
     list_config = lists.getListGenerator(self.request,
-                                         project_view.getParams(), idx=1)
+                                         project_view.getParams(), idx=2)
     return {
         'name': 'projects',
         'title': 'PROJECTS',
+        'lists': Lists([list_config]),
+    }
+
+
+class MyEvaluationsComponent(Component):
+  """Component for listing all the Evaluations of the current Student.
+  """
+
+  def templatePath(self):
+    """Returns the path to the template that should be used in render().
+    """
+    return'v2/modules/gsoc/dashboard/list_component.html'
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    idx = lists.getListIndex(self.request)
+    if idx == 3:
+      list_data = lists.getListData(self.request, project_survey_view.getParams(),
+                                    {'scope': self.data.program})
+      return list_data
+    else:
+      return None
+
+  def context(self):
+    """Returns the context of this component.
+    """
+    list_config = lists.getListGenerator(
+        self.request, project_survey_view.getParams(), idx=3)
+
+    return {
+        'name': 'evaluations',
+        'title': 'EVALUATIONS',
+        'lists': Lists([list_config]),
+    }
+
+
+class SubmittedProposalsComponent(Component):
+  """Component for listing all the Evaluations of the current Student.
+  """
+  # TODO(ljvderijk): Implement
+  pass
+
+
+class ProjectsIMentorComponent(Component):
+  """Component for listing all the Projects mentored by the current user.
+  """
+
+  def templatePath(self):
+    """Returns the path to the template that should be used in render().
+    """
+    return'v2/modules/gsoc/dashboard/list_component.html'
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    idx = lists.getListIndex(self.request)
+    if idx == 4:
+      list_data = lists.getListData(self.request, project_view.getParams(),
+                                    {'program': self.data.program,
+                                     'mentor': self.data.mentors})
+      return list_data
+    else:
+      return None
+
+  def context(self):
+    """Returns the context of this component.
+    """
+    list_config = lists.getListGenerator(
+        self.request, project_view.getParams(), idx=4)
+
+    return {
+        'name': 'mentoring_projects',
+        'title': 'PROJECTS I AM A MENTOR FOR',
+        'lists': Lists([list_config]),
+    }
+
+
+class OrganizationsIAdminComponent(Component):
+  """Component for listing all the Organizations controlled by the current
+  user.
+  """
+
+  def templatePath(self):
+    """Returns the path to the template that should be used in render().
+    """
+    return'v2/modules/gsoc/dashboard/list_component.html'
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    idx = lists.getListIndex(self.request)
+    if idx == 5:
+      # TODO(ljvderijk): Feels weird that if you have access to all the keys
+      # already that you need to use the list code in this way. The rewrite
+      # should probably allow you to control the entities you put in?
+      list_data = lists.getListData(
+          self.request, org_view.getParams(),
+          {'scope': self.data.program,
+           'link_id': [org_admin.scope.link_id
+                       for org_admin in self.data.org_admins]})
+      return list_data
+    else:
+      return None
+
+  def context(self):
+    """Returns the context of this component.
+    """
+    list_config = lists.getListGenerator(
+        self.request, org_view.getParams(), idx=5)
+
+    return {
+        'name': 'adminning_organizations',
+        'title': 'ORGANIZATIONS THAT I AM AN ADMIN FOR',
         'lists': Lists([list_config]),
     }

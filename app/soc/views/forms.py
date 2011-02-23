4600386a1ec87e65d4801919dd8d560fb1952489
Sverre Rabbelier <srabbelier@gmail.com>
2011-02-23 19:33:41 +0100

    Add create method to ModelForm

--- app/soc/views/forms.py
+++ app/soc/views/forms.py
@@ -42,10 +42,12 @@ def choiceWidget(field):
     choices.append((str(choice), unicode(choice)))
   return widgets.Select(choices=choices)
 
+
 def choiceWidgets(model, fields):
   """Returns a dictionary of Select widgets for the specified fields.
   """
   return dict((i, choiceWidget(getattr(model, i))) for i in fields)
+
 
 class ModelForm(djangoforms.ModelForm):
   """Django ModelForm class which uses our implementation of BoundField.
@@ -54,6 +56,45 @@ class ModelForm(djangoforms.ModelForm):
   def __iter__(self):
     for name, field in self.fields.items():
       yield BoundField(self, field, name)
+
+  def create(self, commit=True, key_name=None, parent=None):
+    """Save this form's cleaned data into a new model instance.
+
+    Args:
+      commit: optional bool, default True; if true, the model instance
+        is also saved to the datastore.
+      key_name: the key_name of the new model instance, default None
+      parent: the parent of the new model instance, default None
+
+    Returns:
+      The model instance created by this call.
+    Raises:
+      ValueError if the data couldn't be validated.
+    """
+    if not self.is_bound:
+      raise ValueError('Cannot save an unbound form')
+    opts = self._meta
+    instance = self.instance
+    if self.instance:
+      raise ValueError('Cannot create a saved form')
+    if self.errors:
+      raise ValueError("The %s could not be created because the data didn't "
+                       'validate.' % opts.model.kind())
+    cleaned_data = self._cleaned_data()
+    converted_data = {}
+    for name, prop in opts.model.properties().iteritems():
+      value = cleaned_data.get(name)
+      if value is not None:
+        converted_data[name] = prop.make_value_from_form(value)
+    try:
+      instance = opts.model(key_name=key_name, parent=parent, **converted_data)
+      self.instance = instance
+    except db.BadValueError, err:
+      raise ValueError('The %s could not be created (%s)' %
+                       (opts.model.kind(), err))
+    if commit:
+      instance.put()
+    return instance
 
 
 class Form(object):

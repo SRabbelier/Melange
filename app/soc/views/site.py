23e3c35d6db511742db0e014d3fa298126efcc02
Sverre Rabbelier <srabbelier@gmail.com>
2011-02-27 23:47:23 +0100

    Add reference resolving to edit site settings

--- app/soc/views/site.py
+++ app/soc/views/site.py
@@ -29,12 +29,13 @@ from django.utils.functional import lazy
 from django.utils.translation import ugettext
 
 from soc.logic import cleaning
-from soc.models.work import Work
+from soc.logic.models.document import logic as document_logic
 from soc.models.site import Site
+from soc.models.work import Work
 from soc.modules import callback
 from soc.views.base import SiteRequestHandler
 from soc.views.forms import ModelForm
-from soc.views.helper import widgets
+from soc.views.helper import widgets as widgets_helper
 
 
 def getProgramMap():
@@ -49,22 +50,15 @@ class SiteForm(ModelForm):
 
   class Meta:
     model = Site
-    exclude = ['link_id', 'scope', 'scope_path', 'home', 'tos', 'xsrf_secret_key']
+    exclude = ['link_id', 'scope', 'scope_path', 'home', 'xsrf_secret_key']
     widgets = {
         'active_program': django_widgets.Select(choices=lazy(getProgramMap, list)()),
     }
 
-  home_link_id = widgets.ReferenceField(
-      reference_url='document', required=False,
-      filter_fields={'prefix': 'site'},
-      label=ugettext('Home page Document link ID'),
-      help_text=Work.link_id.help_text)
-
-  tos_link_id = widgets.ReferenceField(
-      reference_url='document', required=False,
-      filter_fields={'prefix': 'site'},
-      label=ugettext('Terms of Service Document link ID'),
-      help_text=Work.link_id.help_text)
+  def clean_tos(self):
+    if self.cleaned_data['tos'] is None:
+      return ''
+    return self.cleaned_data['tos']
 
   clean_noreply_email = cleaning.clean_empty_field('noreply_email')
 
@@ -77,6 +71,16 @@ class SitePage(SiteRequestHandler):
     return [
         url(r'^site/edit$', self, name='edit_site_settings'),
     ]
+
+  def jsonContext(self):
+    entities = document_logic.getForFields({'prefix': 'site'})
+
+    data = [{'key': str(i.key()),
+            'link_id': i.link_id,
+            'title': i.title}
+            for i in entities]
+
+    return {'data': data}
 
   def checkAccess(self):
     # TODO: check is developer

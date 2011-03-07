361e10dc7d23c098b2c2aa1d5daf408a9d980466
Daniel Hans <Daniel.M.Hans@gmail.com>
2011-03-07 13:17:48 +0100

    Very initial version of Proposal page.
    --HG--
    extra : rebase_source : 53c37e8ffae94bf817bbbbdf432ee48ea9f18704

---
+++ app/soc/modules/gsoc/views/proposal.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module for the GSoC proposal page.
+"""
+
+__authors__ = [
+  '"Daniel Hans" <daniel.m.hans@gmail.com>',
+  ]
+
+
+from django.conf.urls.defaults import url
+
+from soc.views import forms
+
+from soc.modules.gsoc.models.student_proposal import StudentProposal
+
+from soc.modules.gsoc.views.base import RequestHandler
+from soc.modules.gsoc.views.helper import url_patterns
+
+
+class ProposalForm(forms.ModelForm):
+  """Django form for the proposal page.
+  """
+
+  class Meta:
+    model = StudentProposal
+    exclude = ['link_id', 'user', 'scope', 'scope_path', 'status',
+        'mentor', 'possible_mentors', 'org', 'program', 'created_on',
+        'last_modified_on', 'score']
+
+  #  widgets = forms.choiceWidgets(StudentProposal,
+  #      ['res_country', 'ship_country',
+  #       'tshirt_style', 'tshirt_size', 'gender'])
+
+
+class ProposalPage(RequestHandler):
+  """View for the submit proposal.
+  """
+
+  def djangoURLPatterns(self):
+    return [
+        url(r'^gsoc/proposal/submit/%s$' % url_patterns.ORG,
+         self, name='submit_gsoc_proposal'),
+    ]
+
+
+  def checkAccess(self):
+    pass
+
+  def templatePath(self):
+    return 'v2/modules/gsoc/proposal/base.html'
+
+  def context(self):
+    proposal_form = user_form = ProposalForm(self.data.POST or None)
+    return {
+        'page_name': 'Proposal',
+        'proposal_form': proposal_form.render(),
+        }

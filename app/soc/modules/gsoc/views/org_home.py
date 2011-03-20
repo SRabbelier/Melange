4de4e9694015d27afe94a7e029f9db751db446fc
Madhusudan.C.S <madhusudancs@gmail.com>
2011-03-21 00:37:37 +0530

    Add a class to render list of projects accepted into an organization.
    Also add the necessary template to render this list.
    
    --HG--
    extra : rebase_source : 84a78e84ce0b4217ed1dcd9735ddd68ab34dcaeb

--- app/soc/modules/gsoc/views/org_home.py
+++ app/soc/modules/gsoc/views/org_home.py
@@ -26,7 +26,9 @@ from django.conf.urls.defaults import url
 
 from soc.views.template import Template
 
+from soc.modules.gsoc.logic.models.student_project import logic as sp_logic
 from soc.modules.gsoc.views.base import RequestHandler
+from soc.modules.gsoc.views.helper import lists
 from soc.modules.gsoc.views.helper import url_patterns
 
 
@@ -44,6 +46,53 @@ class Contact(Template):
 
   def templatePath(self):
     return "v2/modules/gsoc/org_home/_contact.html"
+
+
+class ProjectList(Template):
+  """Template for list of student projects accepted under the organization.
+  """
+
+  def __init__(self, request, data):
+    self.request = request
+    self.data = data
+
+    list_config = lists.ListConfiguration()
+    list_config.addColumn('student', 'Student',
+                          lambda entity, *args: entity.student.user.name)
+    list_config.addSimpleColumn('title', 'Title')
+    list_config.addColumn('mentor', 'Mentor',
+                          lambda entity, *args: entity.mentor.user.name)
+    self._list_config = list_config
+
+  def context(self):
+    list = lists.ListConfigurationResponse(
+        self._list_config, idx=0,
+        description='List of projects accepted into %s' % (
+            self.data.organization.name))
+
+    return {
+        'lists': [list],
+        }
+
+  def getListData(self):
+    """Returns the list data as requested by the current request.
+
+    If the lists as requested is not supported by this component None is
+    returned.
+    """
+    idx = lists.getListIndex(self.request)
+    if idx == 0:
+      fields = {'scope': self.data.organization,
+                'status': 'accepted'}
+      response_builder = lists.QueryContentResponseBuilder(
+          self.request, self._list_config, sp_logic,
+          fields)
+      return response_builder.build()
+    else:
+      return None
+
+  def templatePath(self):
+    return "v2/modules/gsoc/org_home/_project_list.html"
 
 
 class OrgHome(RequestHandler):

5f2af00aa253f15c62b53d671e95ff124482033d
Madhusudan.C.S <madhusudancs@gmail.com>
2011-03-22 02:47:57 +0530

    Tests for Organization home page.

---
+++ tests/views/test_org_home.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Tests for Organization homepage related views.
+"""
+
+__authors__ = [
+  '"Madhusudan.C.S" <madhusudancs@gmail.com>',
+  ]
+
+
+import httplib
+
+from tests.timeline_utils import TimelineHelper
+from tests.profile_utils import GSoCProfileHelper
+from tests.test_utils import DjangoTestCase
+
+# TODO: perhaps we should move this out?
+from soc.modules.seeder.logic.seeder import logic as seeder_logic
+
+
+class OrgHomeViewTest(DjangoTestCase):
+  """Tests organization homepage views.
+  """
+
+  def setUp(self):
+    from soc.models.org_app_survey import OrgAppSurvey
+    from soc.modules.gsoc.models.program import GSoCProgram
+    from soc.modules.gsoc.models.organization import GSoCOrganization
+    from soc.modules.gsoc.models.timeline import GSoCTimeline
+    from soc.modules.gsoc.models.student_project import StudentProject
+
+    properties = {'timeline': seeder_logic.seed(GSoCTimeline)}
+    self.gsoc = seeder_logic.seed(GSoCProgram, properties=properties)
+
+    properties = {'scope': self.gsoc}
+    self.org = seeder_logic.seed(GSoCOrganization, properties=properties)
+
+    self.org_app = seeder_logic.seed(OrgAppSurvey)
+    self.timeline = TimelineHelper(self.gsoc.timeline, self.org_app)
+    self.data = GSoCProfileHelper(self.gsoc)
+
+    properties = {'scope': self.org, 'program': self.gsoc}
+    self.student_projects = seeder_logic.seedn(StudentProject, 2, properties)
+
+  def assertOrgHomeTemplatesUsedBeforeStudentProjectsAnnounced(self, response):
+    """Asserts that all the templates except the one that lists the projects
+    of the organization from the org homepage view were used.
+    """
+    self.assertGSoCTemplatesUsed(response)
+    self.assertTemplateUsed(response, 'v2/modules/gsoc/org_home/base.html')
+    self.assertTemplateUsed(response, 'v2/modules/gsoc/org_home/_contact.html')
+    self.assertTemplateNotUsed(response,
+                            'v2/modules/gsoc/org_home/_project_list.html')
+
+  def assertOrgHomeTemplatesUsedAfterStudentProjectsAnnounced(self, response):
+    """Asserts that all the templates from the org homepage view were used.
+    """
+    self.assertGSoCTemplatesUsed(response)
+    self.assertTemplateUsed(response, 'v2/modules/gsoc/org_home/base.html')
+    self.assertTemplateUsed(response, 'v2/modules/gsoc/org_home/_contact.html')
+    self.assertTemplateUsed(response,
+                            'v2/modules/gsoc/org_home/_project_list.html')
+
+  def testOrgHomeDuringOrgSignup(self):
+    """Tests the the org home page during the organization signup period.
+    """
+    self.timeline.orgSignup()
+    url = '/gsoc/org/' + self.org.key().name()
+    response = self.client.get(url)
+    self.assertOrgHomeTemplatesUsedBeforeStudentProjectsAnnounced(response)
+    self.assertTrue('organization' in response.context)
+    self.assertEqual(response.context['organization'].key(), self.org.key())
+    contact_context = response.context['contact'].context()
+    self.assertTrue('organization' in contact_context)
+    self.assertEqual(contact_context['organization'].key(), self.org.key())
+
+  def testOrgHomeDuringStudentSignup(self):
+    """Tests the the org home page during the student signup period.
+    """
+    self.timeline.studentSignup()
+    url = '/gsoc/org/' + self.org.key().name()
+    response = self.client.get(url)
+    self.assertOrgHomeTemplatesUsedBeforeStudentProjectsAnnounced(response)
+    self.assertTrue('organization' in response.context)
+    self.assertEqual(response.context['organization'].key(), self.org.key())
+    contact_context = response.context['contact'].context()
+    self.assertTrue('organization' in contact_context)
+    self.assertEqual(contact_context['organization'].key(), self.org.key())
+
+  def testOrgHomeAfterStudentProjectsAnnounced(self):
+    """Tests the the org home page after announcing accepted student projects.
+    """
+    self.timeline.studentsAnnounced()
+    url = '/gsoc/org/' + self.org.key().name()
+    response = self.client.get(url)
+    self.assertOrgHomeTemplatesUsedAfterStudentProjectsAnnounced(response)
+    self.assertTrue('organization' in response.context)
+    self.assertEqual(response.context['organization'].key(), self.org.key())
+    contact_context = response.context['contact'].context()
+    self.assertTrue('organization' in contact_context)
+    self.assertEqual(contact_context['organization'].key(), self.org.key())
+    response = self.getListResponse(url, 0)
+    self.assertIsJsonResponse(response)
+
+  def testOrgHomeDuringOffseason(self):
+    """Tests the the org home page after GSoC is over.
+    """
+    self.timeline.offSeason()
+    url = '/gsoc/org/' + self.org.key().name()
+    response = self.client.get(url)
+    self.assertOrgHomeTemplatesUsedAfterStudentProjectsAnnounced(response)
+    self.assertTrue('organization' in response.context)
+    self.assertEqual(response.context['organization'].key(), self.org.key())
+    contact_context = response.context['contact'].context()
+    self.assertTrue('organization' in contact_context)
+    self.assertEqual(contact_context['organization'].key(), self.org.key())
+    response = self.getListResponse(url, 0)
+    self.assertIsJsonResponse(response)

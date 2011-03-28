6bad42c027cbca74542f5a7348664ab585b4ae5e
Sverre Rabbelier <srabbelier@gmail.com>
2011-03-28 22:42:25 +0200

    Fix proposal list

--- app/soc/modules/gsoc/views/dashboard.py
+++ app/soc/modules/gsoc/views/dashboard.py
@@ -428,7 +428,9 @@ class SubmittedProposalsComponent(Component):
     """Initializes this component.
     """
     r = data.redirect
-    list_config = lists.ListConfiguration()
+    list_config = lists.ListConfiguration(add_key_column=False)
+    list_config.addColumn('key', 'Key', (lambda ent, *args: "%s/%s" % (
+        ent.parent().key().name(), ent.key().id())), hidden=True)
     list_config.addSimpleColumn('title', 'Title')
     list_config.addColumn('org', 'Organization',
                           lambda ent, *args: ent.org.name)
